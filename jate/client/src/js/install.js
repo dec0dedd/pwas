e98159c6548f23bb986e3b959e26d428554f07d7
Michał Burzyński <mburzynski@google.com>
2026-08-25 07:30:11 +0000

    Initial dataset

---
+++ jate/client/src/js/install.js
@@ -0,0 +1,32 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Stores the triggered events
+  window.deferredPrompt = event;
+
+  // Removes the hidden class from the button.
+  butInstall.classList.toggle('hidden', false);
+});
+
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  // Shows prompt
+  promptEvent.prompt();
+
+  // Resets the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+
+  // Adds the hidden class to the button.
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  // clear prompt
+  window.deferredPrompt = null;
+});
